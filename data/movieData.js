a4dad5e191f7391463bc113df29e7525c97758cf
Lockett Pundt <kalifornium@hotmail.com>
2020-06-16 16:03:17 -0400

    seperated the mock daata inot it's own directory and file.

---
+++ data/movieData.js
@@ -0,0 +1,16 @@
+const movies = [
+  {
+    id: 1,
+    title: "5 deadly Venoms",
+    releaseDate: "10/10/1983",
+    rating: 5
+  },
+  {
+    id: 2,
+    title: "another movie",
+    releaseDate: "1/08/1976",
+    rating: 3
+  },
+];
+
+module.exports = movies;

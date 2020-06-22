8b6d655ac557481b0eba3a5d232637171288313b
Lockett Pundt <kalifornium@hotmail.com>
2020-06-22 17:35:28 -0400

    added a Movie resolver that returns the actors for the movie selected. added a basic mutation that adds a movie to the static movie data. Added Date scalar type that processes the releaseDate as a new Date() object.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const { ApolloServer, gql } = require('apollo-server-express');
 const express = require('express');
-const movies  = require('../data/movieData');
+const {movies, actors}  = require('../data/movieData');
 const typeDefs = require('../schema/schema');
 const { env } = require('process');
+const { GraphQLScalarType } = require('graphql');
+const { Kind } = require('graphql/language')
 const app = express();
 
 
@@ -16,7 +18,50 @@ const resolvers = {
       })
     }
   },
-}
+  Movie: {
+    actors: (obj, arg, context) => {
+      console.log("actors", obj.actors);
+      const objActors = obj.actors.map(actor => actor.id);
+      // db call or static data reference.
+      const actorProfiles = actors.filter(actor => {
+        return objActors.includes(actor.id);
+      });
+      return actorProfiles;
+    }
+  },
+  
+  Mutation: {
+    addMovie: (obj, { id, releaseDate, title }, context) => {
+      console.log('release date', releaseDate);
+      const newMovieList = [
+        ...movies,
+        {
+          id,
+          releaseDate,
+          title,
+        },
+      ]
+      console.log("new list", newMovieList)
+      return newMovieList;
+    },
+  },
+  
+  Date: new GraphQLScalarType({
+    name: "Date",
+    description: "Date scalar.",
+    parseValue(value) {
+      return new Date(value);
+    },
+    serialize(value) {
+      console.log(value);
+      return new Date(value).getTime();
+    },
+    parseLiteral(ast) {
+      if (ast.kind === Kind.INT) return new Date(ast.value)
+      return null;
+    }
+  })
+};
 
 
 const server = new ApolloServer({

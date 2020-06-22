f9121efbd666d58763bc39677017463b390cadfb
Lockett Pundt <kalifornium@hotmail.com>
2020-06-22 17:37:51 -0400

    added actors to mock data.

--- data/movieData.js
+++ data/movieData.js
@@ -3,18 +3,37 @@ const movies = [
     id: "1",
     title: "5 deadly Venoms",
     releaseDate: "10/10/1983",
-    rating: 5
+    rating: 5,
+    actors: [{
+      id: "22"
+    },
+    {
+      id: "23",
+    }
+    ]
   },
   {
     id: "2",
     title: "another movie",
-    releaseDate: "1/08/1976",
+    releaseDate: "01/08/1976",
     rating: 3,
-    actor: [{
+    actors: [{
       id: "22",
-      name: 'Lockett'
     }]
   },
 ];
 
-module.exports = movies;
+const actors = [
+  {
+    id: "22",
+    name: "Lockett"
+  },
+  {
+    id: "23",
+    name: "Chauncey",
+  },
+];
+
+
+
+module.exports = {movies, actors};

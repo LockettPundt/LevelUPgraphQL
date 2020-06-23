ce204788a0395237783fe27b59e4d0835f8f5364
Lockett Pundt <kalifornium@hotmail.com>
2020-06-22 21:16:07 -0400

    removed the resolver and imported it. added a context to the ApolloServer object where user auth can take place. currently, it returns a fake user Auth.

--- src/index.js
+++ src/index.js
@@ -1,67 +1,11 @@
 const { ApolloServer, gql } = require('apollo-server-express');
 const express = require('express');
-const {movies, actors}  = require('../data/movieData');
+
 const typeDefs = require('../schema/schema');
 const { env } = require('process');
-const { GraphQLScalarType } = require('graphql');
-const { Kind } = require('graphql/language')
+
+const resolvers = require('../resolvers/resolvers');
 const app = express();
-
-
-const resolvers = {
-  Query: {
-    movies: () => movies,
-    movie: (obj, arg, context, info) => {
-      const { id } = arg;
-      return movies.find(movie => {
-        return movie.id === id;
-      })
-    }
-  },
-  Movie: {
-    actors: (obj, arg, context) => {
-      console.log("actors", obj.actors);
-      const objActors = obj.actors.map(actor => actor.id);
-      // db call or static data reference.
-      const actorProfiles = actors.filter(actor => {
-        return objActors.includes(actor.id);
-      });
-      return actorProfiles;
-    }
-  },
-  
-  Mutation: {
-    addMovie: (obj, { id, releaseDate, title }, context) => {
-      console.log('release date', releaseDate);
-      const newMovieList = [
-        ...movies,
-        {
-          id,
-          releaseDate,
-          title,
-        },
-      ]
-      console.log("new list", newMovieList)
-      return newMovieList;
-    },
-  },
-  
-  Date: new GraphQLScalarType({
-    name: "Date",
-    description: "Date scalar.",
-    parseValue(value) {
-      return new Date(value);
-    },
-    serialize(value) {
-      console.log(value);
-      return new Date(value).getTime();
-    },
-    parseLiteral(ast) {
-      if (ast.kind === Kind.INT) return new Date(ast.value)
-      return null;
-    }
-  })
-};
 
 
 const server = new ApolloServer({
@@ -69,6 +13,20 @@ const server = new ApolloServer({
   resolvers,
   introspection: true,
   playground: true,
+  context: ({ req }) => {
+    // user auth can happen here.
+    // returns a user object or grants access for the user
+    // or you could return an error so that a user is blocked from 
+    // posting a mutation.
+    // here is a hardcoded user auth.
+    const fakeUserAuth = {
+      userId: '12345',
+      name: 'lockett',
+    }
+    return {
+      ...fakeUserAuth,
+    };
+  }
 });
 
 server.applyMiddleware({ app, path: '/graphql' });

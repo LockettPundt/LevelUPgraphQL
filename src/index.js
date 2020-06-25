a7ce5060d00e9b67c631c64993e0564edd0e7518
Lockett Pundt <kalifornium@hotmail.com>
2020-06-24 20:13:15 -0400

    added http installSubscriptionHandler for the sunscriptions in express. changed the server.list to httpserver.listen.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
-const { ApolloServer, gql } = require('apollo-server-express');
+const { ApolloServer } = require('apollo-server-express');
 const express = require('express');
+const http = require('http');
 const mongoose = require('mongoose');
 const typeDefs = require('../schema/schema');
 const resolvers = require('../resolvers/resolvers');
+
 const app = express();
 
 mongoose.connect(process.env.DB_URI, {
@@ -13,6 +15,10 @@ mongoose.connect(process.env.DB_URI, {
 })
   .then(() => console.log(`🔥connected to ${process.env.DB_NAME}`))
   .catch((err) => console.log(err));
+
+
+  
+const httpServer = http.createServer(app);
 
 
 const server = new ApolloServer({
@@ -25,7 +31,7 @@ const server = new ApolloServer({
     // returns a user object or grants access for the user
     // or you could return an error so that a user is blocked from 
     // posting a mutation.
-    // here is a hardcoded user auth. :Grapes
+    // here is a hardcoded user auth. 
     const fakeUserAuth = {
       userId: '12345',
       name: 'lockett',
@@ -37,5 +43,7 @@ const server = new ApolloServer({
 });
 
 server.applyMiddleware({ app, path: '/graphql' });
-
-app.listen({port: process.env.PORT || 5000}, () => console.log('🔥connected:  http://localhost:5000/graphql'));
+server.installSubscriptionHandlers(httpServer);
+httpServer.listen({port: process.env.PORT || 5000}, () => {
+  console.log('🔥connected:  http://localhost:5000/graphql')
+});

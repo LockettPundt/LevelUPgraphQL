361ffe67d78a3d90b9fc234ecf30e170d5ae304b
Lockett Pundt <kalifornium@hotmail.com>
2020-06-24 20:16:01 -0400

    added subscriptions to the resolver object. asyncIterator over the new movies added and is passed the sub type of MOVIE_ADDED.

--- resolvers/resolvers.js
+++ resolvers/resolvers.js
@@ -1,19 +1,39 @@
 const { GraphQLScalarType } = require('graphql');
+const { PubSub } = require('apollo-server-express');
 const { Kind } = require('graphql/language')
 const {movies, actors}  = require('../data/movieData');
 const Movie = require('../models/MovieSchema');
+const MOVIE_ADDED = require('../sub_types/subTypes');
+
+const pubsub = new PubSub();
+
 
 const resolvers = {
+  Subscription: {
+    movieAdded: {
+      subscribe: () => pubsub.asyncIterator([MOVIE_ADDED])
+    }
+  },
   Query: {
     movies: async () => {
-      const movieList = await Movie.find();
-      return movieList;
+      try {
+        const movieList = await Movie.find();
+        return movieList;
+      } catch (error) {
+        console.log(error);
+        return [];
+      }
+      
     },
-    movie: (obj, arg, context, info) => {
+    movie: async(obj, arg, context, info) => {
       const { id } = arg;
-      return movies.find(movie => {
-        return movie.id === id;
-      })
+      try {
+        const singleMovie = await Movie.findOne({_id: id });
+        return singleMovie;
+      } catch (error) {
+        console.log(error);
+        return [];
+      }
     }
   },
   Movie: {
@@ -28,31 +48,41 @@ const resolvers = {
   },
   
   Mutation: {
-    addMovie: (obj, { movie }, context) => {
-      const { userId } = context;
-      console.log("checking to see if user has been authorized in the Mutation Query.", userId);
+    addMovie: async (obj, { movie }, context) => {
+      const { userId, name } = context;
+      console.log(`checking to see if ${name} has been authorized in the Mutation Query.`, userId);
       // checks context to see if a valid user.
-      if (userId) {
-        const newMovie = Movie.create({
-          ...movie
-        });
-        return [newMovie]
+      
+      try {
+        if (userId) {
+        
+          const newMovie = await Movie.create({
+            ...movie
+          });
+          pubsub.publish(MOVIE_ADDED, { movieAdded: newMovie });
+          return [newMovie]
+        }
+      } catch (error) {
+        console.log(error);
+        return [];
       }
-      return movies;
     },
     
     updateStatus: async (obj, arg, context) => {
       // context can also be destructured in the argument list.
       const { status, id } = arg;
       const { userId } = context;
-      if (userId) {
-        
-        const movieToUpdate = await Movie.updateOne({_id: id}, { status: status });
-        const movieById = await Movie.findOne({_id: id});
-        console.log( `updating status of ${movieById.title} to ${status}`);
-        return movieById;
+      try {
+        if (userId) {
+          const movieToUpdate = await Movie.updateOne({_id: id}, { status: status });
+          const movieById = await Movie.findOne({_id: id});
+          console.log( `updating status of ${movieById.title} to ${status}`);
+          return movieById;
+        }
+      } catch (error) {
+        console.log(error);
+        return [];
       }
-      return 'sorry charlie';
     },
   },
   
